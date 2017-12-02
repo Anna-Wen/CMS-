0add88f694d92abf6371c414f2d36b6bde82e1b5
Anna-Wen <Anna-Wen@users.noreply.github.com>
2017-12-02 22:49:17 +0800

    Update on Controllers

---
+++ ClassManagementSystem/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ClassManagementSystem.Models;
+
+namespace ClassManagementSystem.Controllers
+{
+    [Produces("application/json")]
+    [Route("")]
+    public class UserController : Controller
+    {
+        User curUser = new User();
+
+        // GET: /me
+        [HttpGet("/me")]
+        public IActionResult GetCurrentUser()
+        {
+            return Json(curUser);
+        }
+
+        // PUT: /me
+        [HttpPut("/me")]
+        public void Put(int id, [FromBody] User editedUser)
+        {
+            
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+    }
+}
