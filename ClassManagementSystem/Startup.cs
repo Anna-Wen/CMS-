d6b36b24302fde9790538f2f73793cb88ebb34f4
Jasperheng <619099139@qq.com>
2017-12-03 15:37:11 +0800

    233

--- ClassManagementSystem/Startup.cs
+++ ClassManagementSystem/Startup.cs
@@ -43,7 +43,7 @@ namespace ClassManagementSystem
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Teacher}/{action=CreateTopic}/{id?}");
+                    template: "{controller=Student}/{action=CourseInfo}/{id?}");
             });
         }
     }
