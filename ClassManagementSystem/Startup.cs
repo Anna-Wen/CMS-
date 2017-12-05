76b3f4908933007a6f035a5503739c9044847b69
Jasperheng <619099139@qq.com>
2017-12-06 01:17:24 +0800

    1:17

--- ClassManagementSystem/Startup.cs
+++ ClassManagementSystem/Startup.cs
@@ -43,7 +43,7 @@ namespace ClassManagementSystem
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=AccountLoginPage}/{id?}");
+                    template: "{controller=Teacher}/{action=TeacherCourseHome}/{id?}");
             });
         }
     }
