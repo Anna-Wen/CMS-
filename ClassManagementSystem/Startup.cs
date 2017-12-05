7082e541a38bee362ffe5e9cb176b818d6b1acf7
Anna-Wen <Anna-Wen@users.noreply.github.com>
2017-12-06 01:18:02 +0800

    Update on SeminarController

--- ClassManagementSystem/Startup.cs
+++ ClassManagementSystem/Startup.cs
@@ -43,11 +43,12 @@ namespace ClassManagementSystem
             {
                 routes.MapRoute(
                     name: "default",
+
+                    template: "{controller=Student}/{action=StudentCourseHome}/{id?}");
+                    //template: "{controller=Teacher}/{action=NewSeminar}/{id?}");
                     //template: "{controller=Teacher}/{action=CheckClassInfo}/{id?}");
                     //template: "{controller=Teacher}/{action=CreateClass}/{id?}");
                     //template: "{controller=Teacher}/{action=CreateCourse}/{id?}");
-                    template: "{controller=Teacher}/{action=CheckTopicBeforeClass}/{id?}");
-                    //template: "{controller=Teacher}/{action=NewSeminar}/{id?}");
                     //template: "{controller=Home}/{action=AccountLoginPage}/{id?}");
             });
         }
