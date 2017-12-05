b802d8ebc65a6909db21ceac7d1db954eee2ec22
loriqing <490855882@qq.com>
2017-12-06 01:21:31 +0800

    zzz

--- ClassManagementSystem/Startup.cs
+++ ClassManagementSystem/Startup.cs
@@ -46,8 +46,8 @@ namespace ClassManagementSystem
                     //template: "{controller=Teacher}/{action=CheckClassInfo}/{id?}");
                     //template: "{controller=Teacher}/{action=CreateClass}/{id?}");
                     //template: "{controller=Teacher}/{action=CreateCourse}/{id?}");
-                    template: "{controller=Teacher}/{action=CheckTopicBeforeClass}/{id?}");
-                    //template: "{controller=Teacher}/{action=NewSeminar}/{id?}");
+                    //template: "{controller=Teacher}/{action=CheckTopicBeforeClass}/{id?}");
+                    template: "{controller=Teacher}/{action=NewSeminar}/{id?}");
                     //template: "{controller=Home}/{action=AccountLoginPage}/{id?}");
             });
         }
